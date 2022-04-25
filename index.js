3f66bbbb4effa7a59923758f654cf9200670270a
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-25 15:07:55 +0600

    server connect with mongodb

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ const port = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 
+//Mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sfale.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        client.connect()
+        console.log("DB connected")
+    } finally {
+
+    }
+}
+run().catch(console.dir)
+
+/* client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
+
+
 app.get('/', (req, res) => {
     res.send(`Port Running at ${port}`)
 })
