4261bb20772a4831075afaf510c0596ac4b018db
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-25 17:29:49 +0600

    order from client side store in db and return all order based on email address

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,7 +12,6 @@ app.use(cors())
 app.use(express.json())
 
 //Mongodb
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sfale.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -20,10 +20,39 @@ const run = async () => {
         client.connect()
         console.log("DB connected")
         const collectonService = client.db("geniusCar").collection('service')
+        const collectonOrder = client.db("geniusCar").collection('order')
         app.get("/service", async (req, res) => {
             const cursor = collectonService.find({});
             const result = await cursor.toArray();
             res.send(result);
+        })
+        app.get("/service/:serviceId", async (req, res) => {
+            const id = req.params.serviceId
+            const query = {
+                _id: ObjectId(id)
+            }
+            const result = await collectonService.findOne(query);
+            ;
+            res.send(result);
+        })
+
+        //order
+        app.post("/order", async (req, res) => {
+            const body = req.body;
+            const result = await collectonOrder.insertOne(body)
+            res.send(result)
+
+        })
+        app.get("/order", async (req, res) => {
+            const queryEmail = req.query.email;
+            const query = {
+                email: queryEmail
+            }
+            const cursor = collectonOrder.find(query)
+            const result = await cursor.toArray();
+
+            res.send(result)
+
         })
     } finally {
 
